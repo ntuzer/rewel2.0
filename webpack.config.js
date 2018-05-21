a89f136d8e71dc66486ffce32af6ae596365247b
ntuzer <rgjr22@gmail.com>
2018-05-21 09:53:40 -0700

    Init 3

--- webpack.config.js
+++ webpack.config.js
@@ -9,13 +9,15 @@ var prodPlugins = [
     'process.env': {
       'NODE_ENV': JSON.stringify('production')
     }
-  }),
-  new webpack.optimize.UglifyJsPlugin({
-    compress: {
-      warnings: true
-    }
   })
 ];
+
+  // ,
+  // new webpack.optimize.UglifyJsPlugin({
+  //   compress: {
+  //     warnings: true
+  //   }
+  // })
 
 plugins = plugins.concat(
   process.env.NODE_ENV === 'production' ? prodPlugins : devPlugins
